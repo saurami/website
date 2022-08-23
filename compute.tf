feab5d8fcbc5637339b9a7fdea0169e0cc80449e
Saurabh Mishra <saurabhm@proton.me>
2022-08-23 14:21:09 -0700

    webserver for statuc files

---
+++ compute.tf
@@ -0,0 +1,54 @@
+provider "google" {
+  project = var.project_id
+  region  = "us-west1"
+}
+
+data "google_service_account" "myaccount" {
+  account_id = var.account_id
+  project    = var.project_id
+}
+
+resource "google_compute_instance" "default" {
+  name                      = "webserver"
+  description               = "Web Server for Personal Website"
+  machine_type              = "f1-micro"
+  zone                      = "us-west1-b"
+  allow_stopping_for_update = true
+  deletion_protection       = false
+
+  tags = ["static-files", "webserver"]
+
+  shielded_instance_config {
+    enable_secure_boot = true
+    enable_vtpm = true
+    enable_integrity_monitoring = true
+  }
+
+  scheduling {
+    provisioning_model = "standard"
+    on_host_maintenance = "TERMINATE"
+    automatic_restart = true
+  }
+
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-minimal-2204-lts-arm64"
+      type = "pd-balanced"
+    }
+  }
+
+  network_interface {
+    network = "default"
+  }
+
+  metadata = {
+    terraform = "true"
+    purpose = "host static files for website"
+  }
+
+  service_account {
+    # Custom service account with restricted permissions
+    email  = data.google_service_account.myaccount.email
+    scopes = ["compute-rw"]
+  }
+}
