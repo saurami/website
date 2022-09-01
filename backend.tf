d8b7261d992afdbcd1c8ceb823aca966ca443690
Saurabh Mishra <saurabhm@proton.me>
2022-08-31 19:00:37 -0700

    create landing page and virtual hosts file

--- backend.tf
+++ backend.tf
@@ -1,21 +1,34 @@
-resource "google_compute_instance_group" "webservers" {
-  description = "Personal wesbite instance group"
-  name        = "saurabh-cc-webservers"
-  network     = "default"
-  zone        = "us-west1-a"
+resource "local_file" "landing_page" {
+  filename = "${path.module}/index.html"
+  file_permission = "0644"
 
-  instances = [
-    google_compute_instance.website_server.id,
-  ]
+  content = <<-EOT
+<html>
+  <head>
+    <title>Welcome to saurabh.cc</title>
+  </head>
+  <body>
+    <h1>Welcome to my personal website !!</h1>
+  </body>
+</html>
+EOT
 
-  named_port {
-    name = "http"
-    port = "8080"
-  }
+}
 
-  named_port {
-    name = "https"
-    port = "8443"
-  }
+
+resource "local_file" "virtual_host" {
+  filename = "${path.module}/saurabh.cc.conf"
+  file_permission = "0400"
+
+  content = <<-EOT
+<VirtualHost *:80>
+  ServerName saurabh.cc
+  ServerAlias www.saurabh.cc
+  ServerAdmin saurabhm@proton.me
+  DocumentRoot /var/www/saurabh.cc/playground
+  ErrorLog $${APACHE_LOG_DIR}/error.log
+  CustomLog $${APACHE_LOG_DIR}/access.log combined
+</VirtualHost>
+EOT
 
 }
